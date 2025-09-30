458c843c4cdacbf43615f19bd7b197985d16fa8b
welivanderkeijl-rgb <welivanderkeijl@live.nl>
2025-09-30 16:18:57 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Welkom bij het Boekingssysteem</h1>
+      <ul>
+        <li><a href="/book">Boeking maken</a></li>
+        <li><a href="/check">In- en uitchecken</a></li>
+      </ul>
+    </main>
+  )
+}
